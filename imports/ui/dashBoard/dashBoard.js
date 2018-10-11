11b9cec460175660017a7808bbdbc17372cc0ec5
amanjot_singh <amanjot.s@successivesoftwares.com>
2018-10-11 12:12:54 +0530

    login todo

--- imports/ui/dashBoard/dashBoard.js
+++ imports/ui/dashBoard/dashBoard.js
@@ -1,9 +1,16 @@
 import './dashBoard.html';
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
+import { Tasks } from '../../api/tasks.js';
+import './task.js';
+
+Template.dashBoard.onCreated(function bodyOnCreated() {
+  Meteor.subscribe('tasks');
+});
+
 
 Template.dashBoard.events({
-  'click'(event) {
+  'click .form-group'(event) {
     event.preventDefault();
     console.log("Logout")
     Meteor.logout(function(err) {
@@ -14,14 +21,36 @@ Template.dashBoard.events({
       }
     });
   },
+
+  'submit .new-task'(event) { 
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Meteor.call('tasks.insert', text);
+
+/*  below commented code is usable only when meteor security is not applied    
+ Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    }); */
+ 
+    // Clear form
+    target.text.value = '';
+  },
 });
+
 Template.dashBoard.helpers({
 cUser : () => {
-
-  return (Meteor.user().username + '<br>') ;
-}
+  return (Meteor.user().username + ',</t>') ;
+},
+tasks() {
+  // Show newest tasks at the top
+  console.log("hello")
+  return Tasks.find({}, { sort: { createdAt: -1 } });
+},
 });
-/* Template.dashBoard.onCreated(function bodyOnCreated() {
-  this.state = new ReactiveDict();
-  Meteor.subscribe('tasks');
-}); */
